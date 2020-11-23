2cf28747798b1f0861cabeaa09b5b19fce07553f
MochaSyrup <yeanjun0222@gmail.com>
2020-11-23 21:47:18 +0900

    2020-11-23 21:47 yyj mpa

---
+++ SpringPRJ/WebContent/LicenseKey/rMateMapChartH5License.js
@@ -0,0 +1,13 @@
+<!--
+// -----------------------------------------------------------------------------
+// rMate MapChart License Key 
+//
+// Product Name : rMate MapChart for HTML5 v5.5
+// License Type : Enterprise Trial License
+// Product No : 37D4-30DC-F203-F49I
+// Authenticated Server Info : undefined
+// Expiration Date : 2021/01/31
+//
+var rMateMapChartH5License = "5e9a79d3068413cb67cccd13520b33a76a5a8b9fa3b4fd651d12b095f361be2e:3000640b633a484244504f2020494c3956343a46322d2e33303020325046562d3a434d4435302d33352d2e3435442d374533563a414c4c20203145334c2f3a317430202f43313a3232303032323a30453120312a303a3348";
+// -----------------------------------------------------------------------------
+// -->
